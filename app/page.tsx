171f15c273bf30d3fd0b32f4211c5928ac3c60b6
Antonio Eggermont <antonioeggermont@Mac.ht.home>
2025-11-24 12:34:51 -0800

    testing static assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 // app/page.tsx
+import Image from "next/image";
+
 export default async function Home() {
   // This fetch will run on the server at request time (SSR)
   const res = await fetch('https://worldtimeapi.org/api/timezone/America/Los_Angeles', {
@@ -7,10 +9,21 @@ export default async function Home() {
   const data = await res.json();
 
   return (
-    <main style={{ padding: '2rem' }}>
-      <h1>My SSR Next.js App on Firebase</h1>
-      <p>Current LA time (from server):</p>
-      <pre>{data.datetime}</pre>
-    </main>
+    <>
+      <main style={{ padding: '2rem' }}>
+        <h1>My SSR Next.js App on Firebase</h1>
+        <p>Current LA time (from server):</p>
+        <pre>{data.datetime}</pre>
+      </main>
+
+      <Image
+        src="/images/AE-Front-Page-Photo.png"
+        alt="Test Image"
+        width={560}
+        height={531}
+      />
+
+    </>
+  
   );
 }
