a9398201554be39f6a5911ace2f5319cc30e32b9
Antonio Eggermont <antonioeggermont@Mac.ht.home>
2025-12-01 12:03:16 -0800

    initial form layout for desktop view

--- app/home/contact-me.tsx
+++ app/home/contact-me.tsx
@@ -4,6 +4,14 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./contact-me.module.scss";
 import Image from "next/image";
 import { backgroundFloatImages } from "./background-float-images";
+import TextField from '@mui/material/TextField';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import emailjs from '@emailjs/browser';
 
 const FLOAT_COUNT = 14;
 
@@ -67,6 +75,40 @@ function AnimatedCardWrapper({
 
 function ContactMe() {
   const [floaters, setFloaters] = useState<FloaterConfig[]>([]);
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    emailjs
+        .sendForm('service_9vnb61i', 'template_4jckt0n', form.current, {
+            publicKey: '3kQLMi8QAvKoYTabS',
+        })
+        .then(
+            () => {
+                alert('A contact request was submitted!');
+                event.target.reset();
+            },
+            (error) => {
+                alert('A contact request was FAILED!');
+                console.log('FAILED...', error.text);
+            },
+        );
+    /*
+    setEmailError(false)
+    if (email == '') {
+        setEmailError(true)
+    }
+    if (email ) {
+        console.log(email)
+    }*/
+
+    
+  };
 
   useEffect(() => {
     // This runs ONLY in the browser, after hydration ✅
@@ -90,6 +132,12 @@ function ContactMe() {
 
     setFloaters(generated);
   }, []);
+
+
+  const handleLinkedIn = () => {
+    window.location.href = 'https://www.linkedin.com/in/antonio-aranda-eggermont-23aa7b8/';
+  };
+
 
   return (
     <section className={styles.contactMeSection} id="contact-me">
@@ -115,10 +163,99 @@ function ContactMe() {
           />
         ))}
       </div>
+
       <div className={styles.content}>
         <h2 className={styles.heading}>Contact Me</h2>
+        <div className={styles.summarySection}>
+          <span className={styles.summarySectionText}> I´m always open to discussing exploring new projects, developments, and partnerships. </span>
+        </div>
 
+        {/*
+
+         
+        <div className={styles.contactFormContainer}>
+          <h1> One</h1>
+          <h1> Two</h1>
+          <h1> Three</h1>
+        </div>     */}
+        {/* Contact form and contact details */}
+       
+        <div className={styles.contactFormContainer}>
+            <div className={styles.writeMeContainer}>
+                <span> Write Me </span>
+                <form ref={form} onSubmit={handleSubmit}>
+                    <div className={styles.formField}>
+                      <TextField
+                        sx={{
+                            width: '32rem',
+                        }}
+                        required
+                        type="text"
+                        name="from_name"
+                        label="Your name"
+                        variant="filled"
+                        onChange={e => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className='form-field'>
+                    <TextField
+                      required
+                      sx={{
+                          width: '32rem',
+                      }}
+                      id="outlined-required"
+                      label="Your email"
+                      type="email"
+                      name="user_email"
+                      defaultValue=""
+                      variant="filled"
+                      onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className='form-field'>
+                      <TextField
+                        required
+                        multiline
+                        rows={4}
+                        sx={{
+                            width: '32rem',
+                        }}
+                        id="outlined-required"
+                        label="Your message"
+                        name="message"
+                        defaultValue=""
+                        variant="filled"
+                        onChange={e => setMessage(e.target.value)}
+                      />
+                    </div>
+                    <div className='form-field-submit'>
+                      <Button type="submit" >Submit</Button>
+                    </div>
+                </form>
+            </div>
+          <div className={styles.contactmeContainer}>
+            <span>Contact Me</span>
+            <div className={styles.contactRow}>
+              <PhoneIphoneIcon sx={{ color: '#02232c' }} style={{ fontSize: 40 }} />
+               <div><span> USA: +206 556 8918</span></div>    
+            </div>
+            <div className={styles.contactRow}>
+              <PhoneIphoneIcon sx={{ color: '#02232c' }} style={{ fontSize: 40 }} />
+              <div><span> Mexico: +52 55 36 71 57 12</span></div>
+            </div>
+            <div className={styles.contactRow}>
+              <AlternateEmailIcon sx={{ color: '#02232c' }} style={{ fontSize: 40 }}/>
+              <div><span> aaeggermont@outlook.com</span></div>
+            </div>
+            <div className={styles.contactRow}>
+              <LinkedInIcon onClick={handleLinkedIn} sx={{ color: '#02232c' }} style={{ fontSize: 40 }} /> 
+              <div><span onClick={handleLinkedIn}> LinkedIn</span></div>
+            </div>
+          </div>
+        </div>
+        
       </div>
+  
     </section>
   );
 }
