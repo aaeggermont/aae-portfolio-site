d7f50559893a971135d8c36d86b71632d4064ac3
Anuar Maruri <anuarmaruri@macbook-pro-de-anuar.jerboa-altered.ts.net>
2025-12-09 11:10:40 -0600

    feat: Added refactored header to project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    additionalData: `
+      @import "@/styles/variables";
+      @import "@/styles/colors";
+      @import "@/styles/mixins";
+    `,
+  },
 };
 
 export default nextConfig;
