b6b4f9c3e88f6c42a844fd3fc26ca3bf6435ebc2
Antonio Eggermont <antonioeggermont@Antonios-MacBook-Pro.local>
2026-01-11 11:21:32 -0600

    initial page upgrade

---
+++ app/mywork/background-float-images.ts
@@ -0,0 +1,6 @@
+import Img1 from "./images/bg-float-1.png";
+import Img2 from "./images/bg-float-2.png";
+import Img3 from "./images/bg-float-3.png";
+import Img4 from "./images/bg-float-4.png";
+
+export const backgroundFloatImages = [Img1, Img2, Img3, Img4];
