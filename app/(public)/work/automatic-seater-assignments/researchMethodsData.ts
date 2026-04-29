498b662826a7b927eb1e9f93d6e9815747062318
Antonio Aranda Eggermont <aarandaeggermont@gmail.com>
2026-04-28 17:10:02 -0700

    removed initial data samples

--- app/(public)/work/automatic-seater-assignments/researchMethodsData.ts
+++
@@ -1,6 +0,0 @@
-import { automaticSeaterAssignmentsDataProject } from "./automatic-seater-assignments.data";
-
-/**
- * @deprecated Import `automaticSeaterAssignmentsDataProject` from `./automatic-seater-assignments.data` instead.
- */
-export const researchMethods = automaticSeaterAssignmentsDataProject.researchMethods;
