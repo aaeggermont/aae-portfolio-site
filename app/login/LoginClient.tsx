899499cc6b9903cc8acd68244b29a5f26dbe195b
Antonio Aranda Eggermont <aarandaeggermont@gmail.com>
2026-04-29 12:59:39 -0700

    included credentials

--- app/login/LoginClient.tsx
+++ app/login/LoginClient.tsx
@@ -22,6 +22,7 @@ export default function LoginClient({ nextPath }: { nextPath: string }) {
     await fetch("/api/auth/session", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
       body: JSON.stringify({ idToken }),
     });
 
