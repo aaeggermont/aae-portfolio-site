0daf12211be755db2343cdb6339d89572f8786b1
Antonio Aranda Eggermont <aarandaeggermont@gmail.com>
2026-05-28 11:53:47 -0700

    refactored Team component to align with the panel container design

--- app/projects/ar-story-teller/components/sections/ProjectOverviewSection.tsx
+++ app/projects/ar-story-teller/components/sections/ProjectOverviewSection.tsx
@@ -9,12 +9,7 @@ interface ProjectOverviewSectionProps {
 export function ProjectOverviewSection({ data }: ProjectOverviewSectionProps) {
     return (
         <section className={styles['project-container']}>
-            <ProjectOverview
-                data={data.projectOverview}
-                data-aos="fade-up"
-                data-aos-duration="1000"
-                data-aos-anchor-placement="top-center"
-            />
+            <ProjectOverview data={data.projectOverview} />
         </section>
     );
 }
