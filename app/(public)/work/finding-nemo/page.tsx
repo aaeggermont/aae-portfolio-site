fa974b0cb1baf530dbeb9929da800e2d5471889b
Antonio Aranda Eggermont <aarandaeggermont@gmail.com>
2026-06-11 15:18:03 -0700

    initial thin page in public space for Finding Nemo

--- app/(public)/work/finding-nemo/page.tsx
+++ app/(public)/work/finding-nemo/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { FindingNemoPage } from "@/app/projects/finding-nemo/FindingNemoPage";
+import ProjectAccessGate from "@/lib/access/ProjectAccessGate";
+import { findingNemoDataProject } from "@/scripts/project-2.data";
 
 export default function FindingNemoRoute() {
-  return <FindingNemoPage />;
+  const { project, gateTitle } = findingNemoDataProject;
+
+  return (
+    <ProjectAccessGate
+      projectId={project.projectId}
+      projectKey={project.projectKey}
+      title={gateTitle}
+    >
+      <FindingNemoPage />
+    </ProjectAccessGate>
+  );
 }
